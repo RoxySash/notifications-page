bfdd179a75b1f3facd8d7e4361c2ef041d3925a3
Roxane Hedge <roxanehedge@gmail.com>
2026-08-01 08:50:45 -0500

    added red notif dots

--- main.js
+++ main.js
@@ -2,18 +2,27 @@ const readBtn = document.getElementById("readBtn");
 const openMessage = document.getElementById("openMessage");
 const dropdownMessage = document.getElementById("dropdownMessage");
 const notifDot = document.getElementById("notifDot");
+const redDot = document.getElementById("redDot");
 
 
 readBtn.addEventListener("click", () => {
-    
+    readMessage();
+    alert("clicked read")
 })
 
 dropdownMessage.addEventListener("click", () => {
 
-    openMessage.toggleAttribute("none");
+    
+  if (openMessage.style.display === "none") {
+    openMessage.style.display = "block";
+
+  } else {
+    openMessage.style.display = "none";
+  }
     
 })
 
-function readMessage() {
-
+function readMessage() {   
+  element.classList.add('read');
 }
+
