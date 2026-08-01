9088fff50ba50eb7d2687c5695e7973a43cb0be7
Roxane Hedge <roxanehedge@gmail.com>
2026-08-01 11:08:46 -0500

    did js for mark read button

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
 const readBtn = document.getElementById("readBtn");
 const openMessage = document.getElementById("openMessage");
 const dropdownMessage = document.getElementById("dropdownMessage");
-const notifDot = document.getElementById("notifDot");
-const redDot = document.getElementById("redDot");
 
+const messageCount = document.getElementById("messageCount");
+const message = document.querySelectorAll(".message");
+const redDot = document.querySelectorAll(".red-dot");
 
-readBtn.addEventListener("click", () => {
-    readMessage();
-    alert("clicked read")
-})
+let count = 7;
+messageCount.innerHTML = count;
 
 dropdownMessage.addEventListener("click", () => {
-
-    
   if (openMessage.style.display === "none") {
     openMessage.style.display = "block";
-
   } else {
     openMessage.style.display = "none";
   }
-    
-})
+});
 
-function readMessage() {   
-  element.classList.add('read');
+function updateCount() {
+  if (message.classList.contains("visited")) {
+    count--;
+    messageCount.innerHTML = count;
+  }
 }
 
+readBtn.addEventListener("click", () => {
+  message.forEach((element) => {
+    element.style.backgroundColor = "white";
+  });
+
+  redDot.forEach((element) => {
+    element.style.display = "none";
+  });
+
+  count = 0;
+  messageCount.innerHTML = count;
+});
