30181b08db2475007df8c880feab70d8b3da630d
Roxane Hedge <roxanehedge@gmail.com>
2026-07-31 23:57:56 -0500

    centered contianer

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const readBtn = document.getElementById("readBtn");
 const openMessage = document.getElementById("openMessage");
 const dropdownMessage = document.getElementById("dropdownMessage");
+const notifDot = document.getElementById("notifDot");
+
 
 readBtn.addEventListener("click", () => {
     
